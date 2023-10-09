3a823bfaecb13bf6bbc1299818049d52bf1b62c2
Maja Golic <mayovita88@gmail.com>
2023-10-09 17:59:59 +0200

    rad na casu

--- js/script.js
+++ js/script.js
@@ -514,6 +514,15 @@ var ime_prezime = ["Marko", "Markovic"];
 
 izracunajProsekIispisiImePrezime(ocene, ime_prezime);
 
+/*Zadatak 5:
+U datoj strukturi se nalazi lista drzava sa svojim kodovima. U drugoj strukturi se nalaze gradovi sa kodovima odredjene drzave.
+Ispisati sve gradove i na osnovu koda drzave ispisati i njeno skraceno ime.
+Npr: Novi Sad - RS, London - UK, Madrid - ESP
+
+var drzave = { 1 : "RS" , 2 : "RU" , 3 : "UK", 4 : "ESP" };
+var gradovi = [ ["London", 3], ["Novi Sad", 1], ["Moskva", 2], ["Madrid", 4]];*/
+
+
 
 
 
